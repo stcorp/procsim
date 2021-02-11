bba8c87fb0252ace23be7003a483c65745eb50d4
Werner Damman <werner.damman@stcorp.nl>
2021-02-11 17:22:09 +0100

    CHG: Added step3 to level0 plugin (just a copy of step2)

--- tasksim/biomass/level0.py
+++ tasksim/biomass/level0.py
@@ -13,7 +13,8 @@ from biomass import mph
 
 
 class Step1():
-    '''Raw slice-based products generation'''
+    '''Raw slice-based products generation. The slice validity start/stop times
+    are set.'''
     def __init__(self, output_path, logger):
         self.output_path = output_path
         self.logger = logger
@@ -31,7 +32,8 @@ class Step1():
 
     def _generate_sliced_output(self, type):
         '''Generate slices for this data take. Slices start and stop on a fixed
-        grid, so the first and last slice of a data take can be truncated.'''
+        grid, aligned to ANX, so the first and last slice of a data take can be
+        truncated.'''
 
         # TODO: Move to constants
         NON_SLICED_TYPES = ['RAWS022_10', 'RAWS023_10', 'RAWS024_10']
@@ -91,7 +93,74 @@ class Step1():
 
 
 class Step2():
-    '''Level-0 slice based products generation'''
+    '''Level-0 slice based products generation. Produce types:
+        - Sx_RAW__0S
+        - Sx_RAWP_0M
+        - RO_RAW__0S
+        - RO_RAWP_0M
+        - EC_RAW__0S
+        - EC_RAW_0M
+    Copies MPH content, but sets the data_take identifier.
+    TODO: Where to get this value from...?'''
+
+    def __init__(self, output_path, logger):
+        self.output_path = output_path
+        self.logger = logger
+        self.input_type = None
+        self.start
+        self.stop
+        self.downlink
+        self.baseline_id = 1
+        self.hdr = mph.MainProductHeader()
+
+    def _generate_bin_file(self, file_name):
+        file = open(file_name, 'w')
+        file.write('test')
+
+    def parse_inputs(self, input_files) -> bool:
+        # Determine input file type, using the directory name.
+        gen = product_name.ProductName()
+        for file in input_files:
+            if gen.parse_path(file):
+                self.input_type = gen.file_type
+                self.start = gen.start_time
+                self.stop = gen.stop_time
+                self.downlink = gen.downlink_time
+                self.baseline_id = gen.baseline_identifier
+            else:
+                self.logger.error('Filename {} not valid for Biomass'.format(file))
+                return False
+        return True
+
+    def generate_outputs(self):
+        output_types = ['S1_RAW__0S', 'S1_RAWP_0M']
+        name_gen = product_name.ProductName()
+        for output_type in output_types:
+            # TODO: Add slice nr, mission phase etc.
+            name_gen.setup(output_type, self.start, self.stop, self.downlink, self.baseline_id)
+
+            # TODO: Just copy from input MPH!
+            self.hdr.eop_identifier = name_gen.generate_path()
+            self.hdr.validity_start = self.start
+            self.hdr.validity_end = self.stop
+            self.hdr.downlink_date = self.downlink
+
+            # Create directory and files
+            dir_name = os.path.join(self.output_path, self.hdr.eop_identifier)
+            os.makedirs(dir_name, exist_ok=True)
+            file_name = os.path.join(dir_name, name_gen.generate_mph_file_name())
+            self.hdr.write(file_name)
+            file_name = os.path.join(dir_name, name_gen.generate_binary_file_name())
+            self._generate_bin_file(file_name)
+            print('Created directory {}'.format(dir_name))
+
+
+class Step3():
+    '''Consolidation of Monitoring products. Produce types:
+        - Sx_RAW__0M
+        - RO_RAW__0M
+        - EC_RAW__0M
+    Copies MPH content, no further actions.'''
     def __init__(self, output_path, logger):
         self.output_path = output_path
         self.logger = logger
@@ -122,9 +191,10 @@ class Step2():
         return True
 
     def generate_outputs(self):
-        output_types = ['S1_RAW__0S', 'S1_RAWP_0M']
+        output_types = ['S1_RAW__0M']
         name_gen = product_name.ProductName()
         for output_type in output_types:
+            # TODO: Add slice nr, mission phase etc.
             name_gen.setup(output_type, self.start, self.stop, self.downlink, self.baseline_id)
 
             # TODO: Just copy from input MPH!
