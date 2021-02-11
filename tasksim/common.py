838b14fcea37a7210274403c0d698a61d0f926d1
Werner Damman <werner.damman@stcorp.nl>
2021-02-11 13:54:28 +0100

    CHG: Added function to remove comments from JSON

--- tasksim/common.py
+++ tasksim/common.py
@@ -4,6 +4,7 @@ Copyright (C) 2021 S[&]T, The Netherlands.
 Common helper functions
 '''
 
+import re
 from xml.etree import ElementTree as et
 
 
@@ -21,3 +22,28 @@ def indent_xml(element, level=0):
     else:
         if level and (not element.tail or not element.tail.strip()):
             element.tail = i
+
+
+def remove_comments(json_like):
+    """
+    Removes C-style comments from *json_like* and returns the result.  Example::
+        >>> test_json = '''\
+        {
+            "foo": "bar", // This is a single-line comment
+            "baz": "blah" /* Multi-line
+            Comment */
+        }'''
+        >>> remove_comments('{"foo":"bar","baz":"blah",}')
+        '{\n    "foo":"bar",\n    "baz":"blah"\n}'
+    """
+    comments_re = re.compile(
+        r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',
+        re.DOTALL | re.MULTILINE
+    )
+
+    def replacer(match):
+        s = match.group(0)
+        if s[0] == '/':
+            return ""
+        return s
+    return comments_re.sub(replacer, json_like)
