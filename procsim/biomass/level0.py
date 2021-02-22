1a6c8268aa53e3f57115b1f5b33a3f62c686ffc6
Werner Damman <werner.damman@stcorp.nl>
2021-02-22 11:52:28 +0100

    CHG: Reworked task table, use single task to slice all products.
    Also replaced the RAWSxxx_10 type by RAWS023, 024 and so on.

--- procsim/biomass/level0.py
+++ procsim/biomass/level0.py
@@ -51,7 +51,7 @@ class ProductGeneratorBase(IProductGenerator):
                 return False
             pattern = self.meta_data_source
             if re.match(pattern, product):
-                self.logger.debug('Using {} as metadata source for {}'.format(os.path.basename(product), self.output_type))
+                self.logger.debug('Parse {} for {}'.format(os.path.basename(product), self.output_type))
                 if gen.parse_path(product):
                     self.input_type = gen.file_type
                     self.start = gen.start_time
@@ -182,7 +182,8 @@ class Sx_RAW__0x_generator(ProductGeneratorBase):
 def OutputGeneratorFactory(logger, job_config, scenario_config) -> Optional[IProductGenerator]:
     generator = None
     product_type = scenario_config['type']
-    if product_type in ['RAWSxxx_10']:
+
+    if product_type in ['RAWS022_10', 'RAWS023_10', 'RAWS024_10', 'RAWS025_10', 'RAWS026_10', 'RAWS027_10', 'RAWS028_10', 'RAWS035_10', 'RAWS036_10']:
         generator = RAWSxxx_10(logger, job_config, scenario_config)
     elif product_type in ['Sx_RAW__0S', 'Sx_RAWP_0M', 'Sx_RAW__0M']:
         generator = Sx_RAW__0x_generator(logger, job_config, scenario_config)
--- test/test.sh
+++ test/test.sh
@@ -23,10 +23,7 @@ mkdir -p workspace
 # --------------------------------
 
 # Step1: Create RAWS_023,024,025,026 products (TODO: All 9 products?)
-python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step1a.xml
-python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step1b.xml
-python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step1c.xml
-python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step1d.xml
+python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step1.xml
 
 # Step2: Create Sx_RAW__0S and Sx_RAWP_0M products (Measurement mode)
 python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step2.xml
