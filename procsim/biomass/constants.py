16b04a254a5aa86c486bd8b0b3750fe978fa6711
Werner Damman <werner.damman@stcorp.nl>
2021-03-02 11:21:09 +0100

    RFT: Renamed ORBIT_DURATION to ORBITAL_PERIOD

--- procsim/biomass/constants.py
+++ procsim/biomass/constants.py
@@ -8,9 +8,9 @@ import datetime
 
 SATELLITE_ID = 'BIO'
 ORBIT0_START = datetime.datetime(2021, 1, 1, 0, 0, 0)    # Just an arbitrary value, to calculate orbit numbers
-ORBIT_DURATION = datetime.timedelta(0, 5890.98)
+ORBITAL_PERIOD = datetime.timedelta(0, 5890.98007989)
 FRAME_GRID_SPACING = datetime.timedelta(0, 19.00316155)
 SLICE_GRID_SPACING = 5 * FRAME_GRID_SPACING
 SLICE_OVERLAP_START = datetime.timedelta(0, 5.0)
 SLICE_OVERLAP_END = datetime.timedelta(0, 7.0)
-SLICES_PER_ORBIT = int(round(ORBIT_DURATION / SLICE_GRID_SPACING))
+SLICES_PER_ORBIT = int(round(ORBITAL_PERIOD / SLICE_GRID_SPACING))
