62c7bb513380cd85c1e2fea71370cecc57033ad0
Werner Damman <werner.damman@stcorp.nl>
2021-03-03 16:40:12 +0100

    CHG: Added test for EC mode. Also renamed processors in tasktable.

--- test/test.sh
+++ test/test.sh
@@ -5,34 +5,60 @@
 # NB: Call from workspace root!
 #-----------------------------------------------------------
 
-# exit when any command fails
+# Exit when any command fails
 set -e
 
-# Generate test data
-mkdir -p data/raw
-python3 procsim/procsim.py -s "Raw data generator" test/procsim_config.json
-
-# Generate aux data
-mkdir -p data/AUX_CN0_AX
-echo test > data/AUX_CN0_AX/AUX_CN0_AXVIEC20030723_000000_19910101_000000_20101231_235959
+# Clean up
+rm -rf data
+rm -rf workspace
 
 # Create workspace root
 mkdir -p workspace
+
 
 # --------------------------------
 # Run test jobs.
 # --------------------------------
 
+# Generate test data
+echo '  *** Generate raw data (measurement mode)'
+mkdir -p data/raw
+python3 procsim/procsim.py -s "Raw data generator, measurement mode" test/procsim_config.json
+
 # Step1: Create RAWS_023,024,025,026 products (TODO: All 9 products?)
-echo '  *** L0 step 1a'
+echo '  *** L0 step 1a (measurement mode)'
 python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step1a.xml
-echo '  *** L0 step 1b'
+echo '  *** L0 step 1b (measurement mode)'
 python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step1b.xml
 
 # Step2 and 3: Create Sx_RAW__0S and Sx_RAW__0M products (Measurement mode)
-echo '  *** L0 step 2,3'
+echo '  *** L0 step 2, 3, 4 (measurement mode)'
 python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step2a.xml
-echo '  *** L0 step 2,3 incomplete slice'
+echo '  *** L0 step 2, 3, 4 incomplete slice (measurement mode)'
 python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step2b.xml
-echo '  *** L0 step 2,3 split slice'
+echo '  *** L0 step 2, 3, 4 split slice (measurement mode)'
 python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step2c.xml
+
+# --------------------------------
+# EC mode
+# --------------------------------
+
+# Generate test data
+echo
+echo '  *** Generate raw data (ec mode)'
+mkdir -p data/raw_ec
+python3 procsim/procsim.py -s "Raw data generator, ec mode" test/procsim_config.json
+
+# Step1: Create RAWS_023,024,025,026 products (TODO: All 9 products?)
+echo '  *** L0 step 1 (ec mode)'
+python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step1a_ec.xml
+# echo '  *** L0 step 1b'
+# python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step1b_ec.xml
+
+# Step2 and 3: Create Sx_RAW__0S and Sx_RAW__0M products (Measurement mode)
+echo '  *** L0 step 2, 3, 4, 5 (ec mode)'
+python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step2a_ec.xml
+# echo '  *** L0 step 2,3 incomplete slice'
+# python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step2b.xml
+# echo '  *** L0 step 2,3 split slice'
+# python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step2c.xml
