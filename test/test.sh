7663f6cd7c9e1ae85db4e2d7192a6ba7a926d996
Werner Damman <werner.damman@stcorp.nl>
2021-03-03 17:18:47 +0100

    CHG: Added RO mode tests

--- test/test.sh
+++ test/test.sh
@@ -25,19 +25,37 @@ echo '  *** Generate raw data (measurement mode)'
 mkdir -p data/raw
 python3 procsim/procsim.py -s "Raw data generator, measurement mode" test/procsim_config.json
 
-# Step1: Create RAWS_023,024,025,026 products (TODO: All 9 products?)
+# Level 0 steps
 echo '  *** L0 step 1a (measurement mode)'
 python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step1a_sm.xml
 echo '  *** L0 step 1b (measurement mode)'
 python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step1b_sm.xml
 
-# Step2 and 3: Create Sx_RAW__0S and Sx_RAW__0M products (Measurement mode)
 echo '  *** L0 step 2, 3, 4 (measurement mode)'
 python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step2a_sm.xml
 echo '  *** L0 step 2, 3, 4 incomplete slice (measurement mode)'
 python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step2b_sm.xml
 echo '  *** L0 step 2, 3, 4 split slice (measurement mode)'
 python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step2c_sm.xml
+
+# --------------------------------
+# RO mode
+# --------------------------------
+
+# Generate test data
+echo
+echo '  *** Generate raw data (ro mode)'
+mkdir -p data/raw_ec
+python3 procsim/procsim.py -s "Raw data generator, ro mode" test/procsim_config.json
+
+# Level 0 steps
+echo '  *** L0 step 1a (ro mode)'
+python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step1a_ro.xml
+echo '  *** L0 step 1b (ro mode)'
+python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step1b_ro.xml
+
+echo '  *** L0 step 2, 3, 4 (ro mode)'
+python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step2a_ro.xml
 
 # --------------------------------
 # EC mode
@@ -49,16 +67,9 @@ echo '  *** Generate raw data (ec mode)'
 mkdir -p data/raw_ec
 python3 procsim/procsim.py -s "Raw data generator, ec mode" test/procsim_config.json
 
-# Step1: Create RAWS_023,024,025,026 products (TODO: All 9 products?)
+# Level 0 steps
 echo '  *** L0 step 1 (ec mode)'
 python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step1a_ec.xml
-# echo '  *** L0 step 1b'
-# python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step1b_ec.xml
 
-# Step2 and 3: Create Sx_RAW__0S and Sx_RAW__0M products (Measurement mode)
 echo '  *** L0 step 2, 3, 4, 5 (ec mode)'
 python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step2a_ec.xml
-# echo '  *** L0 step 2,3 incomplete slice'
-# python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step2b.xml
-# echo '  *** L0 step 2,3 split slice'
-# python pvml/pvml.py test/pvml_config.xml test/pvml_job_biomass_l0_step2c.xml
