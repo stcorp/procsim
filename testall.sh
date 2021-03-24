766bfc9e6d32137c4c356e70d50fc5026d00c4ab
Werner Damman <werner.damman@stcorp.nl>
2021-03-24 11:44:32 +0100

    CHG: Added smoke tests for procsim -v and -h

--- testall.sh
+++ testall.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
+
+# Stop on first error
 set -e
 
+# Unit tests
 cd procsim
 python3 -m unittest
 
+# Test help/version info
+python3 procsim.py -v
+python3 procsim.py -h
+python3 procsim.py -h biomass S1_RAW__0S
+
+# System tests
 cd ..
 test/test.sh
