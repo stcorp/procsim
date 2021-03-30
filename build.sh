19046f54399c3de5211a1582f47df067fb37a240
Werner Damman <werner.damman@stcorp.nl>
2021-03-30 16:50:51 +0200

    FIX: Updated build script, updated package name to BIO_PROCSIM_xxx

--- build.sh
+++ build.sh
@@ -4,19 +4,21 @@
 # This script creates the procsim release package.
 
 # retrieve version number
-VERSION=`grep -Po "__version__[^']*'\K[^']*" procsim/version.py`
+VERSION=`grep -Po "__version__[^']*'\K[^']*" procsim/core/version.py`
 
 # create fresh package dir
-PACKAGE=procsim_$VERSION
+PACKAGE=BIO_PROCSIM_RAW_L0_$VERSION
 rm -rf $PACKAGE
 mkdir $PACKAGE
 
 # copy over release files
 cp LICENSE $PACKAGE
 cp README.md $PACKAGE
+cp MANIFEST.in $PACKAGE
+cp setup.py $PACKAGE
 cp -R procsim $PACKAGE/procsim
 
-# find .git, delete
+# remove development stuff
 cd $PACKAGE
 rm -rf `find . -type d -name .git`
 rm -rf `find . -type d -name __pycache__`
