411bd54b22b458da42dbd5f128f8ccfa6aa58fba
Werner Damman <werner.damman@stcorp.nl>
2021-04-08 13:21:52 +0200

    FIX: No need to add 'python3' before procsim in test script

--- testall.sh
+++ testall.sh
@@ -7,9 +7,9 @@ set -e
 python3 -m unittest discover
 
 # Test help/version info
-python3 procsim -v
-python3 procsim -h
-python3 procsim -h biomass S1_RAW__0S
+procsim -v
+procsim -h
+procsim -i S1_RAW__0S
 
 # System tests
 test/test.sh
