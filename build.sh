cc774612d2a9673ba4a2998d30bf68c0149eab4d
Werner Damman <werner.damman@stcorp.nl>
2023-02-27 13:40:11 +0100

    CHG: 'BIO_' prefix removed from relesse package name

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ set -e
 VERSION=`grep -Po "__version__[^']*'\K[^']*" procsim/core/version.py`
 
 # create fresh package dir
-PACKAGE=BIO_PROCSIM_$VERSION
+PACKAGE=PROCSIM_$VERSION
 rm -rf $PACKAGE
 mkdir $PACKAGE
 
