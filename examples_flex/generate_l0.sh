ec10a68f063083e71a684d9a1df50ee4cc3fe009
Mark Dufour <mark.dufour@stcorp.nl>
2023-03-16 10:16:03 +0100

    merge gen_l0 shell scripts

--- examples_flex/generate_l0.sh
+++ examples_flex/generate_l0.sh
@@ -15,20 +15,20 @@ rm -rf workspace
 mkdir -p workspace
 
 # Generate raw data
-echo '  *** Generate raw data (measurement mode)'
-procsim -s "Raw data generator, measurement mode" procsim_config_raw.json
+#echo '  *** Generate raw data (measurement mode)'
+#procsim -s "Raw data generator, measurement mode" procsim_config_raw.json
 
 # Generate sliced raw data
-echo
-echo '  *** L0 step 1'
-python3 resolve_wildcards.py JobOrder_template.1.xml JobOrder.1.xml
-procsim -t level0_task1.sh -j JobOrder.1.xml procsim_config_l0_step1.json
-echo
-#echo '  *** L0 step 2'
-#python3 resolve_wildcards.py JobOrder_template.2.xml JobOrder.2.xml
-#procsim -t level0_task2.sh -j JobOrder.2.xml procsim_config_l0_step2.json
 #echo
-echo '  *** L0 step 3'
-python3 resolve_wildcards.py JobOrder_template.3.xml JobOrder.3.xml
-procsim -t level0_task3.sh -j JobOrder.3.xml procsim_config_l0_step3.json
+#echo '  *** L0 step 1'
+#python3 resolve_wildcards.py JobOrder_template.1.xml JobOrder.1.xml
+#procsim -t level0_task1.sh -j JobOrder.1.xml procsim_config_l0_step1.json
+#echo
+echo '  *** L0 step 2'
+python3 resolve_wildcards.py JobOrder_template.2.xml JobOrder.2.xml
+procsim -t level0_task2.sh -j JobOrder.2.xml procsim_config_l0_step2.json
 echo
+#echo '  *** L0 step 3'
+#python3 resolve_wildcards.py JobOrder_template.3.xml JobOrder.3.xml
+#procsim -t level0_task3.sh -j JobOrder.3.xml procsim_config_l0_step3.json
+#echo
