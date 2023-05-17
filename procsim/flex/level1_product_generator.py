fa01a567938b030ae2e87e742533917bd6cf1659
Mark Dufour <mark.dufour@stcorp.nl>
2023-05-17 13:38:55 +0200

    now L1B_OBS___

--- procsim/flex/level1_product_generator.py
+++ procsim/flex/level1_product_generator.py
@@ -24,39 +24,7 @@ _HDR_PARAMS = [
 _ACQ_PARAMS = []
 
 
-class ProductGeneratorL1(product_generator.ProductGeneratorBase):
-    INPUTS = []
-
-    def parse_inputs(self, input_products: Iterable[JobOrderInput]) -> bool:
-        # First copy the metadata from any input product (normally H or V)
-        if not super().parse_inputs(input_products, ignore_missing=True):
-            return False
-
-        period_types = collections.defaultdict(set)
-        for input in input_products:
-            if input.file_type[4:] == self._output_type[4:]:
-                for file in input.file_names:
-                    # Skip non-directory products. These have already been parsed in the superclass.
-                    if not os.path.isdir(file):
-                        continue
-                    file, _ = os.path.splitext(file)    # Remove possible extension
-                    gen = product_name.ProductName(self._compact_creation_date_epoch)
-                    gen.parse_path(file)
-                    mph_file_name = os.path.join(file, gen.generate_mph_file_name())
-                    hdr = main_product_header.MainProductHeader()
-                    hdr.parse(mph_file_name)
-                    if hdr.begin_position is None or hdr.end_position is None:
-                        raise ScenarioError('begin/end position not set in {}'.format(mph_file_name))
-                    start = hdr.begin_position
-                    stop = hdr.end_position
-                    self._output_period = (start, stop)
-
-        return True
-
-
-class EO(ProductGeneratorL1):
-    INPUTS = [
-    ]
+class EO(product_generator.ProductGeneratorBase):
 
     PRODUCTS = [
         'L1B_OBS___',
@@ -76,15 +44,72 @@ class EO(ProductGeneratorL1):
 
         self._orbital_period = constants.ORBITAL_PERIOD
 
+        self._output_period: Optional[[datetime.datetime, datetime.datetime]] = None
+
     def get_params(self):
         gen, hdr, acq = super().get_params()
         return gen + self.GENERATOR_PARAMS, hdr + _HDR_PARAMS, acq + _ACQ_PARAMS + self._ACQ_PARAMS
 
+    def parse_inputs(self, input_products: Iterable[JobOrderInput]) -> bool:
+        # First copy the metadata from any input product (normally H or V)
+        if not super().parse_inputs(input_products, ignore_missing=True):
+            return False
+
+        period_types = collections.defaultdict(set)
+        for input in input_products:
+            if input.file_type == 'L0__OBS___':
+                for file in input.file_names:
+                    # Skip non-directory products. These have already been parsed in the superclass.
+                    if not os.path.isdir(file):
+                        continue
+                    file, _ = os.path.splitext(file)    # Remove possible extension
+                    gen = product_name.ProductName(self._compact_creation_date_epoch)
+                    gen.parse_path(file)
+                    mph_file_name = os.path.join(file, gen.generate_mph_file_name())
+                    hdr = main_product_header.MainProductHeader()
+                    hdr.parse(mph_file_name)
+                    if hdr.begin_position is None or hdr.end_position is None:
+                        raise ScenarioError('begin/end position not set in {}'.format(mph_file_name))
+                    start = hdr.begin_position
+                    stop = hdr.end_position
+                    self._output_period = (start, stop)
+
+        return True
+
+
     def generate_output(self):
         super().generate_output()
 
+        if self._output_period is None:
+            return
 
-class CAL(ProductGeneratorL1):
+        start, stop = self._output_period
+        self._logger.debug('EO {} from {} to {}'.format(self._hdr.data_take_id, start, stop))
+
+        # Setup MPH fields. Validity time is not changed, should still be the
+        # theoretical slice start/end.
+        self._hdr.product_type = self._resolve_wildcard_product_type()
+
+        # Create name generator
+        name_gen = self._create_name_generator(self._hdr)
+        name_gen.downlink_time = datetime.datetime.now()  # TODO
+
+        # Create directory and files
+        dir_name = name_gen.generate_path_name()
+        self._hdr.initialize_product_list(dir_name)
+
+        self._logger.info('Create {}'.format(dir_name))
+        dir_name = os.path.join(self._output_path, dir_name)
+        os.makedirs(dir_name, exist_ok=True)
+
+#        file_path = os.path.join(dir_name, name_gen.generate_binary_file_name())
+#        self._add_file_to_product(file_path, self._size_mb // 2)
+
+        file_path = os.path.join(dir_name, name_gen.generate_mph_file_name())
+        self._hdr.write(file_path)
+
+
+class CAL(product_generator.ProductGeneratorBase):
 
     PRODUCTS = [
         'RAC_DARKNP',
@@ -122,6 +147,32 @@ class CAL(ProductGeneratorL1):
         gen, hdr, acq = super().get_params()
         return gen + self.GENERATOR_PARAMS, hdr + _HDR_PARAMS, acq + _ACQ_PARAMS + self._ACQ_PARAMS
 
+    def parse_inputs(self, input_products: Iterable[JobOrderInput]) -> bool:
+        # First copy the metadata from any input product (normally H or V)
+        if not super().parse_inputs(input_products, ignore_missing=True):
+            return False
+
+        period_types = collections.defaultdict(set)
+        for input in input_products:
+            if input.file_type[4:] == self._output_type[4:]:
+                for file in input.file_names:
+                    # Skip non-directory products. These have already been parsed in the superclass.
+                    if not os.path.isdir(file):
+                        continue
+                    file, _ = os.path.splitext(file)    # Remove possible extension
+                    gen = product_name.ProductName(self._compact_creation_date_epoch)
+                    gen.parse_path(file)
+                    mph_file_name = os.path.join(file, gen.generate_mph_file_name())
+                    hdr = main_product_header.MainProductHeader()
+                    hdr.parse(mph_file_name)
+                    if hdr.begin_position is None or hdr.end_position is None:
+                        raise ScenarioError('begin/end position not set in {}'.format(mph_file_name))
+                    start = hdr.begin_position
+                    stop = hdr.end_position
+                    self._output_period = (start, stop)
+
+        return True
+
     def generate_output(self):
         super().generate_output()
 
@@ -138,7 +189,6 @@ class CAL(ProductGeneratorL1):
         # Create name generator
         name_gen = self._create_name_generator(self._hdr)
         name_gen.downlink_time = datetime.datetime.now()  # TODO
-
 
         # Create directory and files
         dir_name = name_gen.generate_path_name()
