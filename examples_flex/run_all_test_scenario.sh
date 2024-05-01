3de56bac994ba2eb08c8d83588a43f5ebd5976ec
Werner Damman <werner.damman@stcorp.nl>
2024-05-01 17:55:50 +0200

    CHG: Replaced FLEX test scenarios with latest version

--- examples_flex/run_all_test_scenario.sh
+++ examples_flex/run_all_test_scenario.sh
@@ -17,6 +17,9 @@ fi
 wait_for_keypress() {
   echo "arg=$no_wait"
   if [ "$no_wait" = false ]; then
+    echo ' '
+    echo ' '
+    echo ' ---------> PRESS a button to continue'
     read -s -n 1
   fi
 }
@@ -57,34 +60,27 @@ mkdir L0Step3_output
 # Generate raw data
 echo ' '
 echo '  ************************************ Generate raw data ************************************ '
-echo ' '
-echo ' '
-echo ' ---------> PRESS a button to continue with:'
+
 echo ' procsim -s "Raw data generator: DATA-DUMP 1" procsim_config_raw.json'
 wait_for_keypress
 echo '  ************************************ Generate raw data for EO DATA-DUMP 1  --> '
 procsim -s "Raw data generator: DATA-DUMP 1" procsim_config_raw.json
-echo ' '
-echo ' '
-echo ' ---------> PRESS a button to continue with:'
+
 echo ' procsim -s "Raw data generator: DATA-DUMP 2" procsim_config_raw.json'
 wait_for_keypress
 echo '  ************************************ Generate raw data for EO DATA-DUMP 2  --> '
 procsim -s "Raw data generator: DATA-DUMP 2" procsim_config_raw.json
-echo ' '
-echo ' '
-echo ' ---------> PRESS a button to continue with:'
+
 echo ' procsim -s "Raw data generator: DATA-DUMP 3" procsim_config_raw.json'
 wait_for_keypress
 echo '  ************************************ Generate raw data for CAL DATA-DUMP  --> '
 procsim -s "Raw data generator: DATA-DUMP 3" procsim_config_raw.json
-echo ' '
-echo ' '
-echo ' ---------> PRESS a button to continue with:'
+
 echo ' procsim -s "Raw data generator: DATA-DUMP 4" procsim_config_raw.json'
 wait_for_keypress
 echo '  ************************************ Generate raw data for CAL DATA-DUMP  --> '
 procsim -s "Raw data generator: DATA-DUMP 4" procsim_config_raw.json
+
 echo ' '
 echo ' '
 echo '  ************************************ GENERATED RAW DATA:  ************************************ '
@@ -94,186 +90,194 @@ echo ' '
 echo ' '
 echo '  ************************************ L0 STEP 1 ************************************ '
 echo ' '
-echo ' '
-echo ' ---------> PRESS a button to continue with L0 STEP 1'
 echo ' L0 STEP 1: RAW HR1 DATA DUMP 1 --> ./level0_step1.sh JobOrder.L0PF_S1_RAW-HR1_D1.xml'
 wait_for_keypress
 echo ' '
 echo '  ************************************ L0 STEP 1: RAW HR1 DATA DUMP 1 --> '
-./level0_step1.sh Scenario1_EO/JobOrder.L0PF_S1_RAW-HR1_D1.xml
-echo ' '
-echo ' '
-echo ' ---------> PRESS a button to continue with:'
+./level0_step1.sh Scenario/JobOrder.L0PF_S1_RAW-HR1_D1.xml
+
 echo ' L0 STEP 1: RAW HR2 DATA DUMP 1 --> ./level0_step1.sh JobOrder.L0PF_S1_RAW-HR2_D1.xml'
 wait_for_keypress
 echo ' '
 echo '  ************************************ L0 STEP 1: RAW HR2 DATA DUMP 1 --> '
-./level0_step1.sh Scenario1_EO/JobOrder.L0PF_S1_RAW-HR2_D1.xml
-echo ' '
-echo ' '
-echo ' ---------> PRESS a button to continue with:'
+./level0_step1.sh Scenario/JobOrder.L0PF_S1_RAW-HR2_D1.xml
+
 echo ' L0 STEP 1: RAW LR DATA DUMP 1 --> ./level0_step1.sh JobOrder.L0PF_S1_RAW-LR__D1.xml'
 wait_for_keypress
 echo ' '
 echo '  ************************************ L0 STEP 1: RAW LR DATA DUMP 1 --> '
-./level0_step1.sh Scenario1_EO/JobOrder.L0PF_S1_RAW-LR__D1.xml
-echo ' '
-echo ' '
-echo ' ---------> PRESS a button to continue with:'
+./level0_step1.sh Scenario/JobOrder.L0PF_S1_RAW-LR__D1.xml
+
 echo ' L0 STEP 1: RAW HR1 DATA DUMP 2 --> ./level0_step1.sh JobOrder.L0PF_S1_RAW-HR1_D2.xml'
 wait_for_keypress
 echo ' '
 echo '  ************************************ L0 STEP 1: RAW HR1 DATA DUMP 2 --> '
-./level0_step1.sh Scenario1_EO/JobOrder.L0PF_S1_RAW-HR1_D2.xml
-echo ' '
-echo ' '
-echo ' ---------> PRESS a button to continue with:'
+./level0_step1.sh Scenario/JobOrder.L0PF_S1_RAW-HR1_D2.xml
+
 echo ' L0 STEP 1: RAW HR2 DATA DUMP 2 --> ./level0_step1.sh JobOrder.L0PF_S1_RAW-HR2_D2.xml'
 wait_for_keypress
 echo ' '
 echo '  ************************************ L0 STEP 1: RAW HR2 DATA DUMP 2 --> '
-./level0_step1.sh Scenario1_EO/JobOrder.L0PF_S1_RAW-HR2_D2.xml
-echo ' '
-echo ' '
-echo ' ---------> PRESS a button to continue with:'
+./level0_step1.sh Scenario/JobOrder.L0PF_S1_RAW-HR2_D2.xml
+
 echo ' L0 STEP 1: RAW LR DATA DUMP 2 --> ./level0_step1.sh JobOrder.L0PF_S1_RAW-LR__D2.xml'
 wait_for_keypress
 echo ' '
 echo '  ************************************ L0 STEP 1: RAW LR DATA DUMP 2 --> '
-./level0_step1.sh Scenario1_EO/JobOrder.L0PF_S1_RAW-LR__D2.xml
-echo ' '
-echo ' '
-echo ' ---------> PRESS a button to continue with: WARNING NEVER TRIED BEFORE!!!'
+./level0_step1.sh Scenario/JobOrder.L0PF_S1_RAW-LR__D2.xml
+
 echo ' L0 STEP 1: RAW OBC DATA DUMP 1 --> ./level0_step1.sh JobOrder.L0PF_S1_RAW-OBC_D1.xml'
 wait_for_keypress
 echo ' '
 echo '  ************************************ L0 STEP 1: RAW OBC DATA DUMP 1 --> '
-./level0_step1.sh Scenario1_EO/JobOrder.L0PF_S1_RAW-OBC_D1.xml
-echo ' '
-echo ' '
-echo ' ---------> PRESS a button to continue with:'
+./level0_step1.sh Scenario/JobOrder.L0PF_S1_RAW-OBC_D1.xml
+
 echo ' L0 STEP 1: RAW OBC DATA DUMP 2 --> ./level0_step1.sh JobOrder.L0PF_S1_RAW-LR__D2.xml'
 wait_for_keypress
 echo ' '
 echo '  ************************************ L0 STEP 1: RAW OBC DATA DUMP 2 --> '
-./level0_step1.sh Scenario1_EO/JobOrder.L0PF_S1_RAW-OBC_D2.xml
+./level0_step1.sh Scenario/JobOrder.L0PF_S1_RAW-OBC_D2.xml
+
 echo ' '
 echo ' '
 echo ' '
 echo '  ************************************ L0 STEP 2 ************************************ '
-echo ' '
-echo ' '
-echo ' ---------> PRESS a button to continue with L0 STEP 2'
 echo ' L0 STEP 2: HR2 Slice 3 --> ./level0_step2.sh JobOrder.L0PF_S2_RWS-HR2-Slice3.xml'
 wait_for_keypress
 echo ' '
 echo '  ************************************ L0 STEP 2: HR2 Slice 3 --> '
-./level0_step2.sh Scenario1_EO/JobOrder.L0PF_S2_RWS-HR2-Slice3.xml
-echo ' '
-echo ' '
-echo ' ---------> PRESS a button to continue with:'
+./level0_step2.sh Scenario/JobOrder.L0PF_S2_RWS-HR2-Slice3.xml
+
 echo ' L0 STEP 2: HR1 Slice 7 --> ./level0_step2.sh JobOrder.L0PF_S2_RWS-HR1-Slice7.xml'
 wait_for_keypress
 echo ' '
 echo '  ************************************ L0 STEP 2: HR1 Slice 7  --> '
-./level0_step2.sh Scenario1_EO/JobOrder.L0PF_S2_RWS-HR1-Slice7.xml
-echo ' '
-echo ' '
-echo ' ---------> PRESS a button to continue with:'
+./level0_step2.sh Scenario/JobOrder.L0PF_S2_RWS-HR1-Slice7.xml
+
 echo ' L0 STEP 2: HR2 VAU - orbit 1024 --> ./level0_step2.sh JobOrder.L0PF_S2_RWS-HR2-VAU-1024.xml'
 wait_for_keypress
 echo ' '
 echo '  ************************************ L0 STEP 2: HR2 VAU - orbit 1024  --> '
-./level0_step2.sh Scenario1_EO/JobOrder.L0PF_S2_RWS-HR2-VAU-1024.xml
-echo ' '
-echo ' '
-echo ' ---------> PRESS a button to continue with:'
+./level0_step2.sh Scenario/JobOrder.L0PF_S2_RWS-HR2-VAU-1024.xml
+
 echo ' L0 STEP 2: LR VAU - orbit 1024 --> ./level0_step2.sh JobOrder.L0PF_S2_RWS-LR_-VAU-1024.xml'
 wait_for_keypress
 echo ' '
 echo '  ************************************ L0 STEP 2: LR VAU - orbit 1024  --> '
-./level0_step2.sh Scenario1_EO/JobOrder.L0PF_S2_RWS-LR_-VAU-1024.xml
+./level0_step2.sh Scenario/JobOrder.L0PF_S2_RWS-LR_-VAU-1024.xml
+
+echo ' L0 STEP 2: Additional joborders'
+wait_for_keypress
+echo ' '
+./level0_step2.sh Scenario/JobOrder.L0PF_S2_RWS-HR1-SliceCAL3.xml
+./level0_step2.sh Scenario/JobOrder.L0PF_S2_RWS-HR1-VAU-1025.xml
+./level0_step2.sh Scenario/JobOrder.L0PF_S2_RWS-HR1-VAU-1026.xml
+./level0_step2.sh Scenario/JobOrder.L0PF_S2_RWS-HR2-SliceCAL2.xml
+./level0_step2.sh Scenario/JobOrder.L0PF_S2_RWS-HR2-VAU-1025.xml
+./level0_step2.sh Scenario/JobOrder.L0PF_S2_RWS-HR2-VAU-1026.xml
+./level0_step2.sh Scenario/JobOrder.L0PF_S2_RWS-LR_-VAU-1025.xml
+./level0_step2.sh Scenario/JobOrder.L0PF_S2_RWS-LR_-VAU-1026.xml
+./level0_step2.sh Scenario/JobOrder.L0PF_S2_RWS-XS_ITM-1024.xml
+./level0_step2.sh Scenario/JobOrder.L0PF_S2_RWS-XS_ITM-1025.xml
+./level0_step2.sh Scenario/JobOrder.L0PF_S2_RWS-XS_ITM-1026.xml
+./level0_step2.sh Scenario/JobOrder.L0PF_S2_RWS-XS_OBC-1025.xml
+./level0_step2.sh Scenario/JobOrder.L0PF_S2_RWS-XS_OBC-1026.xml
+
 echo ' '
 echo ' '
 echo ' '
 echo '  ************************************ L0 STEP 3 ************************************ '
-echo ' '
-echo ' '
-echo ' ---------> PRESS a button to continue with L0 STEP 3'
 echo ' L0 STEP 3: Slice 1 --> ./level0_step3.sh JobOrder.L0PF_S3_RWS-1.xml'
 wait_for_keypress
 echo ' '
 echo '  ************************************ L0 STEP 3: Slice 1 --> '
-./level0_step3.sh Scenario1_EO/JobOrder.L0PF_S3_RWS-1.xml
-echo ' '
-echo ' '
-echo ' ---------> PRESS a button to continue with:'
+./level0_step3.sh Scenario/JobOrder.L0PF_S3_RWS-1.xml
+
 echo ' L0 STEP 3: Slice 2 --> ./level0_step3.sh JobOrder.L0PF_S3_RWS-2.xml'
 wait_for_keypress
 echo ' '
 echo '  ************************************ L0 STEP 3: Slice 2 --> '
-./level0_step3.sh Scenario1_EO/JobOrder.L0PF_S3_RWS-2.xml
-echo ' '
-echo ' '
-echo ' ---------> PRESS a button to continue with:'
+./level0_step3.sh Scenario/JobOrder.L0PF_S3_RWS-2.xml
+
 echo ' L0 STEP 3: Slice 3 --> ./level0_step3.sh JobOrder.L0PF_S3_RWS-3.xml'
 wait_for_keypress
 echo ' '
 echo '  ************************************ L0 STEP 3: Slice 3 --> '
-./level0_step3.sh Scenario1_EO/JobOrder.L0PF_S3_RWS-3.xml
-echo ' '
-echo ' '
-echo ' ---------> PRESS a button to continue with:'
+./level0_step3.sh Scenario/JobOrder.L0PF_S3_RWS-3.xml
+
 echo ' L0 STEP 3: Slice 4 --> ./level0_step3.sh JobOrder.L0PF_S3_RWS-4.xml'
 wait_for_keypress
 echo ' '
 echo '  ************************************ L0 STEP 3: Slice 4 --> '
-./level0_step3.sh Scenario1_EO/JobOrder.L0PF_S3_RWS-4.xml
-echo ' '
-echo ' '
-echo ' ---------> PRESS a button to continue with:'
+./level0_step3.sh Scenario/JobOrder.L0PF_S3_RWS-4.xml
+
 echo ' L0 STEP 3: Slice 5 --> ./level0_step3.sh JobOrder.L0PF_S3_RWS-5.xml'
 wait_for_keypress
 echo ' '
 echo '  ************************************ L0 STEP 3: Slice 5 --> '
-./level0_step3.sh Scenario1_EO/JobOrder.L0PF_S3_RWS-5.xml
-echo ' '
-echo ' '
-echo ' ---------> PRESS a button to continue with:'
+./level0_step3.sh Scenario/JobOrder.L0PF_S3_RWS-5.xml
+
 echo ' L0 STEP 3: Slice 6 --> ./level0_step3.sh JobOrder.L0PF_S3_RWS-6.xml'
 wait_for_keypress
 echo ' '
 echo '  ************************************ L0 STEP 3: Slice 6 --> '
-./level0_step3.sh Scenario1_EO/JobOrder.L0PF_S3_RWS-6.xml
-echo ' '
-echo ' '
-echo ' ---------> PRESS a button to continue with:'
+./level0_step3.sh Scenario/JobOrder.L0PF_S3_RWS-6.xml
+
 echo ' L0 STEP 3: Slice 7 --> ./level0_step3.sh JobOrder.L0PF_S3_RWS-7.xml'
 wait_for_keypress
 echo ' '
 echo '  ************************************ L0 STEP 3: Slice 7 --> '
-./level0_step3.sh Scenario1_EO/JobOrder.L0PF_S3_RWS-7.xml
-echo ' '
-echo ' '
-echo ' ---------> PRESS a button to continue with:'
+./level0_step3.sh Scenario/JobOrder.L0PF_S3_RWS-7.xml
+
 echo ' L0 STEP 3: Slice 8 --> ./level0_step3.sh JobOrder.L0PF_S3_RWS-8.xml'
 wait_for_keypress
 echo ' '
 echo '  ************************************ L0 STEP 3: Slice 8 --> '
-./level0_step3.sh Scenario1_EO/JobOrder.L0PF_S3_RWS-8.xml
-echo ' '
-echo ' '
-echo ' ---------> PRESS a button to continue with:'
+./level0_step3.sh Scenario/JobOrder.L0PF_S3_RWS-8.xml
+
 echo ' L0 STEP 3: Slice 9 --> ./level0_step3.sh JobOrder.L0PF_S3_RWS-9.xml'
 wait_for_keypress
 echo ' '
 echo '  ************************************ L0 STEP 3: Slice 9 --> '
-./level0_step3.sh Scenario1_EO/JobOrder.L0PF_S3_RWS-9.xml
-echo ' '
-echo ' '
-echo ' ---------> PRESS a button to continue with:'
-echo ' L0 STEP 3: VAU orbit 1024 --> ./level0_step3.sh JobOrder.L0PF_S3_RWS-VAU-1024.xml'
+./level0_step3.sh Scenario/JobOrder.L0PF_S3_RWS-9.xml
+
+echo ' L0 STEP 3: Slice 9 --> ./level0_step3.sh JobOrder.L0PF_S3_RWS-9.xml'
 wait_for_keypress
 echo ' '
-echo '  ************************************ L0 STEP 3: Slice VAU - orbit 1024 --> '
-./level0_step3.sh Scenario1_EO/JobOrder.L0PF_S3_RWS-VAU-1024.xml
+echo '  ************************************ L0 STEP 3: Slice 9 --> '
+./level0_step3.sh Scenario/JobOrder.L0PF_S3_RWS-9.xml
+
+# Added 2024/05/01
+echo ' '
+echo '  ************************************ L0 STEP 3: Additional steps --> '
+echo ' '
+./level0_step3.sh Scenario/JobOrder.L0PF_S3_RWS-CAL248.xml
+./level0_step3.sh Scenario/JobOrder.L0PF_S3_RWS-CAL249.xml
+./level0_step3.sh Scenario/JobOrder.L0PF_S3_RWS-CAL250.xml
+./level0_step3.sh Scenario/JobOrder.L0PF_S3_RWS-VAU-1024.xml
+./level0_step3.sh Scenario/JobOrder.L0PF_S3_RWS-VAU-1025.xml
+./level0_step3.sh Scenario/JobOrder.L0PF_S3_RWS-VAU-1026.xml
+./level0_step3.sh Scenario/JobOrder.L0PF_S3_RWS-XS_ITM-1024.xml
+./level0_step3.sh Scenario/JobOrder.L0PF_S3_RWS-XS_ITM-1025.xml
+./level0_step3.sh Scenario/JobOrder.L0PF_S3_RWS-XS_ITM-1026.xml
+./level0_step3.sh Scenario/JobOrder.L0PF_S3_RWS-XS_OBC-1024.xml
+./level0_step3.sh Scenario/JobOrder.L0PF_S3_RWS-XS_OBC-1025.xml
+./level0_step3.sh Scenario/JobOrder.L0PF_S3_RWS-XS_OBC-1026.xml
+
+echo ' '
+echo ' '
+echo ' '
+echo '  ************************************ L1 ************************************ '
+wait_for_keypress
+./level1.sh Scenario/JobOrder.L1PF_CAL_1.xml
+./level1.sh Scenario/JobOrder.L1PF_CAL_2.xml
+./level1.sh Scenario/JobOrder.L1PF_CAL_3.xml
+./level1.sh Scenario/JobOrder.L1PF_OBS_1.xml
+./level1.sh Scenario/JobOrder.L1PF_OBS_2.xml
+./level1.sh Scenario/JobOrder.L1PF_OBS_3.xml
+./level1.sh Scenario/JobOrder.L1PF_OBS_4.xml
+./level1.sh Scenario/JobOrder.L1PF_OBS_5.xml
+./level1.sh Scenario/JobOrder.L1PF_OBS_6.xml
+./level1.sh Scenario/JobOrder.L1PF_OBS_7.xml
+./level1.sh Scenario/JobOrder.L1PF_OBS_8.xml
+./level1.sh Scenario/JobOrder.L1PF_OBS_9.xml
--- examples_flex/run_cal_test_scenario.sh
+++ examples_flex/run_cal_test_scenario.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -x
-./level0_step1.sh Scenario2_CAL/JobOrder.L0PF_S1_RAW-HR1_D3.xml
-./level0_step1.sh Scenario2_CAL/JobOrder.L0PF_S1_RAW-HR1_D4.xml
-./level0_step1.sh Scenario2_CAL/JobOrder.L0PF_S1_RAW-HR2_D3.xml
-./level0_step1.sh Scenario2_CAL/JobOrder.L0PF_S1_RAW-HR2_D4.xml
-./level0_step1.sh Scenario2_CAL/JobOrder.L0PF_S1_RAW-LR__D3.xml
-./level0_step1.sh Scenario2_CAL/JobOrder.L0PF_S1_RAW-LR__D4.xml
-./level0_step1.sh Scenario2_CAL/JobOrder.L0PF_S1_RAW-OBC_D3.xml
-./level0_step1.sh Scenario2_CAL/JobOrder.L0PF_S1_RAW-OBC_D4.xml
+./level0_step1.sh Scenario/JobOrder.L0PF_S1_RAW-HR1_D3.xml
+./level0_step1.sh Scenario/JobOrder.L0PF_S1_RAW-HR1_D4.xml
+./level0_step1.sh Scenario/JobOrder.L0PF_S1_RAW-HR2_D3.xml
+./level0_step1.sh Scenario/JobOrder.L0PF_S1_RAW-HR2_D4.xml
+./level0_step1.sh Scenario/JobOrder.L0PF_S1_RAW-LR__D3.xml
+./level0_step1.sh Scenario/JobOrder.L0PF_S1_RAW-LR__D4.xml
+./level0_step1.sh Scenario/JobOrder.L0PF_S1_RAW-OBC_D3.xml
+./level0_step1.sh Scenario/JobOrder.L0PF_S1_RAW-OBC_D4.xml
 
-./level0_step3.sh Scenario2_CAL/JobOrder.L0PF_S3_RWS-1.xml
-./level0_step3.sh Scenario2_CAL/JobOrder.L0PF_S3_RWS-2.xml
-./level0_step3.sh Scenario2_CAL/JobOrder.L0PF_S3_RWS-3.xml
+./level0_step3.sh Scenario/JobOrder.L0PF_S3_RWS-1.xml
+./level0_step3.sh Scenario/JobOrder.L0PF_S3_RWS-2.xml
+./level0_step3.sh Scenario/JobOrder.L0PF_S3_RWS-3.xml
