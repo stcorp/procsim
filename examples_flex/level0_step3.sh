769e882ea56bd55e07634b636c9133ff0a27c3f4
Werner Damman <werner.damman@stcorp.nl>
2024-05-13 09:49:20 +0200

    RFT: Removed wrong comments

--- examples_flex/level0_step3.sh
+++ examples_flex/level0_step3.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-#--------------------------------------------------------------------------
-# S&T procsim demo.
-#
-# The generated files have the creation date in their file name.
-# The JobOrder templates have wildcards for the creation date part, the
-# 'resolve_wildcards' tool fills in the actual file names.
-#--------------------------------------------------------------------------
 
 # Exit when any command fails
 set -e
